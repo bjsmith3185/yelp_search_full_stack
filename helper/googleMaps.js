d21255888a4976a0cb86d119f290822ff35b6ddd
bjsmith3185 <bjsmith3185@hotmail.com>
2018-10-26 11:55:10 -0400

    cleaned up the file tree

--- helper/googleMaps.js
+++ helper/googleMaps.js
@@ -1,31 +1,4 @@
 
-var googleMapsClient = require('@google/maps').createClient({
-    key: "AIzaSyA6pItobxq0v_r7pWG5w_R36jtaVw8h520",
-  });
-
-  module.exports = function googleMap(address) {
-     
-    return new Promise((resolve, reject) => {
-
-    googleMapsClient.geocode({
-        // center: {lat: lat, lng: long},
-        address: address
-      }, function(err, response) {
-        if (!err) {
-            // console.log("this is google maps");
-            // console.log(response.json.results);
-    
-        } else {
-            console.log("this is not google maps");
-            console.log(err);
-        }
-        resolve(response.json.results)
-      });
-     
-
-    });
-
-  }
   
   
   
